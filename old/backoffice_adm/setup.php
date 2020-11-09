df0ade3ccfce77e044fe7c8ed1f504db7d3c3260
munkgorn <munk.gorn@gmail.com>
2020-11-09 13:24:41 +0700

    SET BUG

--- old/backoffice_adm/setup.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	define('BASE', $base);
-	define('BASE_FOL', $base.'adminFsoftpro88/');  
-	define('BASE_CATALOG', $base.'catalog/');
-	define('THEME','theme');
-	define('BASE_ASSET', MURL.'assets/'.THEME.'/');
-?>
